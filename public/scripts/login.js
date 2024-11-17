ebb9f9cef573eac3057153a4e5096c890bf4cf86
00071222 <00071222@uca.edu.sv>
2024-11-17 00:08:41 -0600

    server test

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -25,7 +25,7 @@ document.getElementById("login").addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const form = new FormData(e.target);
-    const response = await fetch('https://localhost/residencial/?action=login', {
+    const response = await fetch('https://192.168.231.129/residencial/?action=login', {
         headers: { 'Content-Type': 'application/json' },
         method: 'POST',
         body: JSON.stringify({
@@ -36,7 +36,7 @@ document.getElementById("login").addEventListener('submit', async (e) => {
     const result = await response.json();
     if (response.status === 200) {
         alert(result.message);
-        window.location.href = 'https://localhost/residencial/?action=redirect';
+        window.location.href = 'https://192.168.231.129/residencial/?action=redirect';
     } else if (response.status === 400) {
         alert(`Error ${response.status}: ${JSON.stringify(result.message)}`)
     }
@@ -49,7 +49,7 @@ document.getElementById("signup").addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const form = new FormData(e.target);
-    const response = await fetch('https://localhost/residencial/?action=signup', {
+    const response = await fetch('https://192.168.231.129/residencial/?action=signup', {
         headers: { 'Content-Type': 'application/json' },
         method: 'POST',
         body: JSON.stringify({
