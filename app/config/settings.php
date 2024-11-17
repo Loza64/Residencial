6fc65cff54fcd3b23341285bc5dae2151864d4e2
Loza64 <robertoloxa88@gmail.com>
2024-11-17 13:36:27 -0600

    changes

--- app/config/settings.php
+++ app/config/settings.php
@@ -10,17 +10,20 @@ class Settings
     private $pass;
     private $mode;
     private $domain;
+    private $dbhost;
     private $dbport;
 
     public function __construct()
     {
         Dotenv::createImmutable('./')->load();
+        //APP
+        $this->domain = $_ENV["DOMAIN"] ?? "localhost";
+        $this->mode = $_ENV["MODE"] ?? null;
+
+        //DATABASE
         $this->user = $_ENV["USER"] ?? null;
         $this->pass = $_ENV["PASS"] ?? null;
-        $this->mode = $_ENV["MODE"] ?? null;
-        $this->dbport = $_ENV["DBPORT"] ?? 3306;
-        $this->domain = $_ENV["DOMAIN"] ?? null;
-        $this->dsn = "mysql:host={$this->domain};port={$this->dbport};dbname=residencial;charset=utf8mb4";
+        $this->dsn = "mysql:host=" . ($_ENV['HOST'] ?? 'localhost') . ";port=3306;dbname=residencial;charset=utf8mb4";;
     }
 
     public function getDsn()
